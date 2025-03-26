4229dc40b59b8eb7b3ce68295fe8a541eba4cb3b
Nicolas Webb <nicw103@gmail.com>
2025-03-26 18:24:00 +0200

    Made the notes clickable and added thirdparty ID generator

--- app.js
+++ app.js
@@ -13,10 +13,47 @@ class Note {
 class App {
     constructor() {
         this.notes = [];
+        
+        this.$activeform = document.querySelector(".active-form");
+        this.$inactiveform = document.querySelector(".inactive-form");
+        this.noteTitle = document.querySelector("#note-title")
+        this.noteText = document.querySelector("#note-text")
+
+        this.addEventListeners();
+    }
+    // Event Listeners
+    addEventListeners() {
+        document.body.addEventListener("click", (event) => {
+            this.handleFormClick(event);
+        })
+    }
+
+    handleFormClick(event) {
+    const isActiveFormClickedOn = this.$activeform.contains(event.target);
+    const isInactiveFormClickedOn = this.$inactiveform.contains(event.target);
+    const title = this.noteTitle.value;
+    const text = this.noteText.value;
+
+    if (isInactiveFormClickedOn) {
+        this.openActiveForm();
+    } else if (!isInactiveFormClickedOn && !isActiveFormClickedOn) {
+        this.closeActiveForm();
+    }
+    };
+
+    openActiveForm() {
+        this.$inactiveform.style.display = "none";
+        this.$activeform.style.display = "block";
+        this.noteText.focus();
+    }
+
+    closeActiveForm() {
+        this.$inactiveform.style.display = "block";
+        this.$activeform.style.display = "none";
     }
 
     // Adds a note
-    addNote(id, {title, text}) {
+    addNote({title, text}) {
         const newNote = new Note(id,title,text);
         this.notes.push(newNote);
     }
